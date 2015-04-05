4a46612eaf73f4218a7ecacd362fc7137d7ae66c
Lucas de Levy Oliveira <lucasdelevy@gmail.com>
2015-04-05 18:53:18 -0300

    Comments now have owner

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -71,6 +71,6 @@ class CommentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
-      params.require(:comment).permit(:post_id, :body)
+      params.require(:comment).permit(:post_id, :body, :name)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150405201444) do
+ActiveRecord::Schema.define(version: 20150405212103) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "post_id"
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name"
   end
 
   create_table "posts", force: :cascade do |t|
